7b86154f5a2269ca3979f64f8ecaeb23c046a2ed
吴俊城 <wujunchengman@163.com>
2022-05-24 22:26:29 +0800

    添加项目文件。

---
+++ KiteTools.GuidGenerate/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace KiteTools.GuidGenerate.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KiteTools.GuidGenerateWPF/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KiteTools.GuidGenerateWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void generate_Click(object sender, RoutedEventArgs e)
+        {
+            if (onlyGuid.IsChecked.GetValueOrDefault())
+            {
+                generateValue.Text = Guid.NewGuid().ToString();
+            }else if(idGuid.IsChecked.GetValueOrDefault()){
+                generateValue.Text= $"ID = new Guid(\"{ Guid.NewGuid()}\")," ;
+            }
+            else if (newGuid.IsChecked.GetValueOrDefault())
+            {
+                generateValue.Text = $"new Guid(\"{ Guid.NewGuid()}\")";
+            }
+
+
+            Clipboard.SetDataObject(generateValue.Text);
+        }
+    }
+}
